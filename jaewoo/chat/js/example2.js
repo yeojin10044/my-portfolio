37b2101f1b98c38100f6fb5ed0bca2fabb27a620
unbreakablesh <dlwodnc8@gmail.com>
2024-01-16 12:06:49 +0900

    test용입니다.

---
+++ jaewoo/chat/js/example2.js
@@ -0,0 +1,11 @@
+// document.write("hello")
+
+
+
+    function openNewWindow() {
+    // 새 창의 크기를 지정합니다. 여기서는 폭 500px, 높이 300px로 설정했습니다.
+    var windowFeatures = "width=600,height=900";
+
+    // 새 창을 엽니다.
+    window.open("http://www.naver.com", "_blank", windowFeatures);
+}
---
+++ jaewoo/js/chatBot.js
@@ -0,0 +1,11 @@
+// document.write("hello")
+
+
+
+    function openNewWindow() {
+    // 새 창의 크기를 지정합니다. 여기서는 폭 500px, 높이 300px로 설정했습니다.
+    var windowFeatures = "width=506,height=651";
+
+    // 새 창을 엽니다.
+    window.open("chat/newPage2.html", "_blank", windowFeatures);
+}
