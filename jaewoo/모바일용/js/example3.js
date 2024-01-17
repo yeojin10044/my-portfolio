7091e239f6143a8b64316fd29b51176844d1e84b
unbreakablesh <dlwodnc8@gmail.com>
2024-01-17 18:53:44 +0900

    모바일용  업데이트 헤헷 쳇봇은 다음생에에

---
+++ jaewoo/모바일용/js/example3.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    document.getElementById("main_right").onclick = function () {
+        alert("쿠폰 모두 소진 ");
+    };
+
+    document.getElementById("main_center").onclick = function () {
+        alert("새로운 소식 0 ");
+    };
+
+
+
+});
